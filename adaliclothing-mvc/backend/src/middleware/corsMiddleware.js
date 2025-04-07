3ea925f1e269062c7cfac7ba65175a65bdd3747d
Mutyii0425 <106231360+Mutyii0425@users.noreply.github.com>
2025-04-07 21:38:02 +0200

    Update corsMiddleware.js

--- adaliclothing-mvc/backend/src/middleware/corsMiddleware.js
+++ adaliclothing-mvc/backend/src/middleware/corsMiddleware.js
@@ -1,8 +1,11 @@
 import cors from 'cors';
 
 const corsOptions = {
-  origin: 'http://localhost:3000', 
-  optionsSuccessStatus: 200
+  origin: ['http://localhost:3000', 'https://adaliclothing.vercel.app'],
+  optionsSuccessStatus: 200,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+  credentials: true
 };
 
 export default cors(corsOptions);
